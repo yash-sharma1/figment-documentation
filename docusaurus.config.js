bf20c190174143681b59a7746242ffa2931ee001
paulpomerleau <78575161+paulpomerleau@users.noreply.github.com>
2022-09-01 14:41:31 -0400

    fix api-reference service routes (#55)
    * fix slash routes on api-ref services

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const githubConfig =
   process.env.NODE_ENV === "development"
     ? {
-        baseUrl: "/",
+        baseUrl: "/test/",
       }
     : {
         baseUrl: "/figment-documentation/",
